262211a0ea5ee68463951e79949d3a3ee8037ead
Nguyenvuh2r <nguyenvuh2r@gmail.com>
2024-06-26 22:12:04 +0700

    fixed

--- src/views/pages/blog/post/PostForm.vue
+++ src/views/pages/blog/post/PostForm.vue
@@ -232,7 +232,7 @@ function ckUploader(editor) {
   // didn't work on es6 arrow function :'(
   // ex: const ckUploader = () => {} 
   editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-    return new CkUploadAdapterPlugin(loader, route.params.id, 'site-page')
+    return new CkUploadAdapterPlugin(loader, route.params.id, 'blog-post')
   }
 }
 const editor = DocumentEditor
